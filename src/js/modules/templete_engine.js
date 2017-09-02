93690578a58ebd623c705d8563c43fffce8f43c1
kikuchi <usushio193@gmail.com>
2017-09-02 17:21:18 +0900

    [fix] for文対応

--- src/js/modules/templete_engine.js
+++ src/js/modules/templete_engine.js
@@ -1,6 +1,6 @@
 
 
-//var code = ["___age___","___ShopName___","___Name___"];
+//var code = ["___age___","___ShopName___","___Name___","___ID___"];
 
 
 var users =[
@@ -8,27 +8,33 @@ var users =[
           id:1,
           name:"Sudo",
           age:21,
-          sex:"man"
+          sex:"man",
+          shopname:"ion"
         },
         {
           id:2,
           name:"Sususu",
           age:23,
-          sex:"man"
+          sex:"man",
+          shopname:"kinokuni"
         }
     ];
 
+var checktext = "___Name___のたこてあき___age___あああ___ShopName___";
 
-    var checktext = "___Name___のたこてあき___age___あああ___ShopName___";
+for(var i = 0; i < 3; i++){
 
-  //  for(var i = 0; i < 3; i++){
+  var result = checktext.match(/(___.*?___)/);
 
-    var result = checktext.match(/(___.*?___)/);
-
-    console.log(result);
-    if(result[0] === "___Name___"){
-       checktext =checktext.replace(/___Name___/,users[0].name);
-       console.log(checktext);
-    }
-
-//}
+  console.log(result);
+  if(result[0] === "___Name___"){
+    checktext =checktext.replace(/___Name___/,users[0].name);
+    console.log(checktext);
+  }else if(result[0] === "___age___"){
+    checktext =checktext.replace(/___age___/,users[0].age);
+    console.log(checktext);
+  }else if(result[0] === "___ShopName___"){
+    checktext =checktext.replace(/___ShopName___/,users[0].shopname);
+    console.log(checktext);
+  }
+}
