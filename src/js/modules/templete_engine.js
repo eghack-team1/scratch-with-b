40f8dad354362531d6a347ed4af3127ccace1a4f
kikuchi <usushio193@gmail.com>
2017-09-02 16:55:06 +0900

    [fix] text判別

--- src/js/modules/templete_engine.js
+++ src/js/modules/templete_engine.js
@@ -2,13 +2,36 @@
 
 //var code = ["___age___","___ShopName___","___Name___"];
 
-
-    var checktext = "___age___のたこてあき___Name___あああ___ShopName___";
-
-    for(var i = 0; i < 3; i++){
-
-    var checktext = checktext.replace(/___.*?___/, "Christmas");
-
-    console.log(checktext);
+var users_data =[
+  users:[{
+        user:{
+          id:1,
+          name:"Sudo",
+          age:21,
+          sex:man
+        },
+        {
+          id:2,
+          name:"Sususu",
+          age:23,
+          sex:man
+        },
+      }
+    ]
   }
+];
+
+    var checktext = "___Name___のたこてあき___age___あああ___ShopName___";
+
+  //  for(var i = 0; i < 3; i++){
+
+    var result = checktext.match(/(___.*?___)/);
+
+    if(result == "___Name___"){
+      console.users_data[1].
+    }
+
+
+    console.log(result);
+//}
   console.log(checktext);
