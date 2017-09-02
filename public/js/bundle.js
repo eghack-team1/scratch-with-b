0a95709b16772958f91c7ac41639a16ef570cc2f
Yuta <user@user-no-MacBook-Air.local>
2017-09-03 01:09:24 +0900

    [add] for-statement,if-statement,variable-block

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -96,20 +96,47 @@ class ScratchBlockController {
 
   dragStartHandler(event) {
     let dragElement = event.target;
-    draggingDOM = dragElement.cloneNode(true);
+    switch(dragElement.getAttribute('data-scratch-type')) {
+      case null:
+        return;
+        break;
+      case 'workspace':
+        return;
+        break;
+      case 'box':
+        event.stopPropagation();
+        return;
+        break;
+      default:
+        break;
+    }
+    draggingDOM = dragElement;
   }
 
   dragOverHandler(event) {
       event.preventDefault();
       event.dataTransfer.dropEffect = 'move';
-
   }
 
   dropHandler(event) {
       let dropElement = event.target;
-      event.stopPropagation();
-      console.error(draggingDOM);
-      dropElement.appendChild(draggingDOM);
+      switch (dropElement.getAttribute('data-scratch-type')){
+        case 'workspace':
+          event.stopPropagation();
+          draggingDOM.parentNode.removeChild(draggingDOM);
+          break;
+        case 'box':
+          event.stopPropagation();
+          dropElement.appendChild(draggingDOM.cloneNode(true));
+          break;
+        case 'command-wrapper-block':
+          event.stopPropagation();
+          console.error(draggingDOM);
+          dropElement.appendChild(draggingDOM.cloneNode(true));
+          break;
+        default:
+          return;
+      }
   }
 }
 /* harmony export (immutable) */ __webpack_exports__["a"] = ScratchBlockController;
