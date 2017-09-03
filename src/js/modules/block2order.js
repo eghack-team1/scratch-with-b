3216a0ebce32a4b4e6d48d4e5c14f01bfd787042
Hiroyuki Abe <mysterious08bubble03@gmail.com>
2017-09-03 10:19:53 +0900

    bug fixed and speeding up(maybe)

--- src/js/modules/block2order.js
+++ src/js/modules/block2order.js
@@ -1,82 +1,56 @@
-/* DOMから送られてきたオブジェクトの state 属性で関数呼び出し */
+/**
+ * call_function
+ *  |- state          <string> : block state
+ *  |- l_side, r_side <object> : left and right side variable
+ *  |- operator       <string> : comparison operator
+ *  |- contable       <boolean>: is the left side variable countable?
+ */
 function call_state(call_function) {
-  /* 右辺・左辺の値をローカル変数に落としておく */
-  r_side = call_function.r_side;
-  l_side = call_function.l_side;
+  r_side = call_function.r_side, l_side = call_function.l_side;
 
-  /* 各 state ごとに命令呼び出し */
-  if (call_function.state === "if") { /* if state */
-    /* 各比較演算子ごとに見てみる */
-    switch (call_function.command) {
+  if (call_function.state === "if") {
+    switch (call_function.operator) {
       case "==":
-        if (l_side === r_side) {
-          console.log("イコールだお");
-          return true;
-        } else {
-          console.log("イコールじゃないお");
-          return false;
-        }
+        return (l_side === r_side ? true : false);
         break;
       case ">":
-        if (l_side > r_side) {
-          console.log("左辺のほうが大きいお(´・ω・｀)");
-          return true;
-        }
+        return (l_side > r_side ? true : false);
         break;
       case "<":
-        if (l_side < r_side) {
-          console.log("右辺のほうが大きいお(´・ω・｀)");
-          console.log(call_function.l_side, call_function.r_side);
-          return true;
-        }
+        return (l_side < r_side ? true : false);
         break;
       case ">=":
-        if (l_side >= r_side) {
-          console.log(l_side, "is bigger. (>=)");
-          return true;
-        }
+        return (l_side >= r_side ? true : false);
         break;
       case "<=":
-        if (l_side <= r_side) {
-          console.log(r_side, "is bigger. (<=)");
-          return true;
-        }
+        return (l_side <= r_side ? true : false);
         break;
-      default:
-        console.log("そんなものはない");
+      default: /* just in case ... */
+        console.warn("illegal operator :(");
+        return false;
     }
   }
 
-  if (call_function.state === "loop") {  /* loop state */
+  if (call_function.state === "loop") {
     if (call_function.contable === true) {
-      /* カウントできるタイプのものは for 文で回す */
+      /* countable -> for loop */
       for (var i = 0; i < l_side.length; i++) {
         console.log(l_side[i]);
+        // document.write(l_side[i]);
       }
     } else {
-      /* カウントできないものは while で回す */
+      /* uncountable -> while loop */
       call_function.state = "if";
-      console.log("while入りマース");
       while (!call_state(call_function)) {
-        console.log("しんどい");
         call_function.l_side += 1;
+        console.log(call_function.l_side);
+        // document.write(l_side[i]);
       }
     }
   }
 }
 
-age = 24;
-var call_function = new Object();
-// call_function.state   = "if";
-// call_function.r_side  = age;
-// call_function.l_side  = 10;
-// call_function.command = ">";
-
-call_function.state    = "loop";
-call_function.r_side   = 5;
-call_function.l_side   = 0;
-call_function.l_side   = ["ヘラクレス", "清姫", "ポール・バニヤン", "ダレイオス三世", "世界"];
-call_function.command  = ">";
-call_function.contable = true;
-
-call_state(call_function);
+/* to translate <type>, if necessary */
+// $.data(element, key, value); /* to get $.data */
+call_function = new Object();
+call_state(call_function)
